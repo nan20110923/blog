09ff1fbaf3fec1f0c252ae86257ee0f1cc0a78de
NaN <nan20110923@outlook.com>
2025-01-04 21:00:09 +0800

    ~ use gray-matter for parsing mdx metadata

--- src/app/posts/utils.ts
+++ src/app/posts/utils.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import matter from 'gray-matter'
 
 type Metadata = {
   title: string
@@ -8,41 +9,23 @@ type Metadata = {
   image?: string
 }
 
-function parseFrontmatter(fileContent: string) {
-  const frontmatterRegex = /---\s*([\s\S]*?)\s*---/
-  const match = frontmatterRegex.exec(fileContent)
-  const frontMatterBlock = match![1]
-  const content = fileContent.replace(frontmatterRegex, '').trim()
-  const frontMatterLines = frontMatterBlock.trim().split('\n')
-  const metadata: Partial<Metadata> = {}
-
-  frontMatterLines.forEach((line) => {
-    const [key, ...valueArr] = line.split(': ')
-    let value = valueArr.join(': ').trim()
-    value = value.replace(/^['"](.*)['"]$/, '$1') // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value
-  })
-
-  return { metadata: metadata as Metadata, content }
-}
-
 function getMDXFiles(dir: fs.PathLike) {
   return fs.readdirSync(dir).filter((file) => path.extname(file) === '.mdx')
 }
 
 function readMDXFile(filePath: fs.PathOrFileDescriptor) {
   const rawContent = fs.readFileSync(filePath, 'utf-8')
-  return parseFrontmatter(rawContent)
+  return matter(rawContent)
 }
 
 function getMDXData(dir: fs.PathLike) {
   const mdxFiles = getMDXFiles(dir)
   return mdxFiles.map((file) => {
-    const { metadata, content } = readMDXFile(path.join(dir.toString(), file))
+    const { data, content } = readMDXFile(path.join(dir.toString(), file))
     const slug = path.basename(file, path.extname(file))
 
     return {
-      metadata,
+      metadata: data,
       slug,
       content,
     }
